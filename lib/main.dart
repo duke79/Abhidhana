03eb9e64d794a68cbc675251e535722ace4612dd
Pulkit Singh <pulkitsingh01@gmail.com>
2018-03-10 21:02:21 +0530

    Localization | final

--- lib/main.dart
+++ lib/main.dart
@@ -7,49 +7,53 @@ import 'package:myapp/routes/Search.dart';
 
 void main() {
   //Run app
-  runApp(new MaterialApp(
-      title: 'Vilokan Dictionary',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      supportedLocales: [
-        const Locale('en', ''), //English
-        const Locale('es', ''), //Spanish
-      ],
-      localizationsDelegates: [
-        // ... app-specific localization delegate[s] here
-/*        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,*/
-        DefaultMaterialLocalizations.delegate,
-        const MyLocalizationDelegate(),
-      ],
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case '/' :
-            return new MyCustomRoute(
-              builder: (context) =>
-              new Home(title: MyLocale
-                  .of(context)
-                  .title /*'Flutter Demo Home Page'*/),
-              settings: settings,
-            );
-          case '/search':
-            bool firstBuild = true;
-            FocusNode focusNode = new FocusNode();
-            return new MyCustomRoute(
-              builder: (BuildContext context) {
-                Widget ret = new Search(focusNode: focusNode,);
-                if (true == firstBuild) {
-                  FocusScope.of(context).requestFocus(focusNode);
-                  firstBuild = false;
-                }
-                return ret;
-              },
-              settings: settings,
-            );
-        }
-      },
-    ),);
+  runApp(myApp());
   //Cache heavy data
   DatabaseServices.trie;
+}
+
+Widget myApp(){
+  return new MaterialApp(
+    title: 'Vilokan Dictionary',
+    theme: new ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    supportedLocales: [
+      const Locale('en', ''), //English
+      const Locale('es', ''), //Spanish
+    ],
+    localizationsDelegates: [
+      // ... app-specific localization delegate[s] here
+/*        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,*/
+      DefaultMaterialLocalizations.delegate,
+      const MyLocalizationDelegate(),
+    ],
+    onGenerateRoute: (RouteSettings settings) {
+      switch (settings.name) {
+        case '/' :
+          return new MyCustomRoute(
+            builder: (context) =>
+            new Home(title: MyLocale
+                .of(context)
+                .title /*'Flutter Demo Home Page'*/),
+            settings: settings,
+          );
+        case '/search':
+          bool firstBuild = true;
+          FocusNode focusNode = new FocusNode();
+          return new MyCustomRoute(
+            builder: (BuildContext context) {
+              Widget ret = new Search(focusNode: focusNode,);
+              if (true == firstBuild) {
+                FocusScope.of(context).requestFocus(focusNode);
+                firstBuild = false;
+              }
+              return ret;
+            },
+            settings: settings,
+          );
+      }
+    },
+  );
 }
