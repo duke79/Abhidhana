acb3a9e7d3f33e312565f5c776f923b24faebed6
Pulkit Singh <pulkitsingh01@gmail.com>
2018-03-11 20:41:31 +0530

    Route | Result | definition positioned

--- lib/routes/Result.dart
+++ lib/routes/Result.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:myapp/data/DatabaseServices.dart';
 import 'package:myapp/data/Strings.dart';
+import 'package:myapp/model/Screen.dart';
 
 class ResultState extends State<Result> {
   String _definition = "";
@@ -23,44 +24,52 @@ class ResultState extends State<Result> {
           child: new Column(
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              new Container(
-                decoration: new ShapeDecoration(
-                  //color: Colors.blue[500],
-                  gradient: new LinearGradient(
-                    colors: <Color>[
-                      Colors.blue[900],
-                      Colors.blue[500]
-                    ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.center,
-                  ),
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.all(
-                        new Radius.circular(10.0),
+              new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new Container(
+                      decoration: new ShapeDecoration(
+                        //color: Colors.blue[500],
+                        gradient: new LinearGradient(
+                          colors: <Color>[
+                            Colors.blue[900],
+                            Colors.blue[500]
+                          ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.center,
+                        ),
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.all(
+                            new Radius.circular(10.0),
+                          ),
+                        ),
+                        shadows: <BoxShadow>[
+                          new BoxShadow(
+                            color: const Color(0xcc000000),
+                            offset: new Offset(0.0, 2.0),
+                            blurRadius: 2.0,
+                          ),
+                        ],
                       ),
-                  ),
-                  shadows: <BoxShadow>[
-                    new BoxShadow(
-                      color: const Color(0xcc000000),
-                      offset: new Offset(0.0, 2.0),
-                      blurRadius: 2.0,
+                      padding: const EdgeInsets.only(
+                        left: 50.0,
+                        top: 12.0,
+                        bottom: 12.0,
+                      ),
+                      child: new Text(
+                        widget.word,
+                        style: const TextStyle(color: Colors.white),
+                        textScaleFactor: 3.0,
+                      ),
                     ),
-                  ],
-                ),
-                child: new AnimatedAlign(
-                  alignment: Alignment.lerp(
-                      Alignment.centerLeft, Alignment.center, 0.15),
-                  duration: const Duration(seconds: 1),
-                  child: new Text(
-                    widget.word,
-                    style: const TextStyle(color: Colors.white),
-                    textScaleFactor: 3.0,
                   ),
-                ),
+                ],
               ),
-              new AnimatedAlign(
-                alignment: Alignment.center,
-                duration: const Duration(seconds: 1),
+              new Container(
+                margin: new EdgeInsets.only(
+                  top: 20.0,
+                  left: 100.0,
+                ),
                 child: _definition == "" ? new CircularProgressIndicator()
                     : new Text(
                   _definition,
