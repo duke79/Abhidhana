ec9fdadf918362902e89ab67b8d1d333ff617ef3
Pulkit Singh <pulkitsingh01@gmail.com>
2018-03-21 22:17:00 +0530

    UI | Animation | drag

--- lib/routes/Result.dart
+++ lib/routes/Result.dart
@@ -9,23 +9,25 @@ class ResultState extends State<Result> with SingleTickerProviderStateMixin {
 
   AnimationController _animationController;
   Animation<num> _animation;
+  Offset _dragDelta = new Offset(0.0, 0.0);
 
   @override
   void initState() {
     _animationController = new AnimationController(
-        duration: const Duration(seconds: 20),
-        vsync: this,
+      duration: const Duration(seconds: 20),
+      vsync: this,
     );
     _animation =
         new Tween(begin: 600.0, end: 0.0).animate(_animationController);
     _animation.addListener(() {
       setState(() => null);
     });
-    _animationController.animateWith(new SpringSimulation(new SpringDescription.withDampingRatio(
-      mass: 2.0,
-      stiffness: 2.0,
-      ratio: 1.0
-    ),0.0,1.0,1.0));
+    _animationController.animateWith(
+        new SpringSimulation(new SpringDescription.withDampingRatio(
+            mass: 2.0,
+            stiffness: 2.0,
+            ratio: 1.0
+        ), 0.0, 1.0, 1.0));
     super.initState();
   }
 
@@ -46,10 +48,10 @@ class ResultState extends State<Result> with SingleTickerProviderStateMixin {
                 children: <Widget>[
                   new Container(
                     margin: new EdgeInsets.only(
-                      top: _animation.value.toDouble()/2,
+                      top: _animation.value.toDouble() / 2,
                     ),
                     //Because PageView doesn't work under flex widgets (Column,Row)
-                    height: _animation.value.toDouble()/2+200,
+                    height: _animation.value.toDouble() / 2 + 200,
                     child: new PageView.builder(
                       physics: new AlwaysScrollableScrollPhysics(),
                       controller: _controller,
@@ -61,29 +63,37 @@ class ResultState extends State<Result> with SingleTickerProviderStateMixin {
                       },
                     ),
                   ),
-                  new Container(
-                    margin: new EdgeInsets.only(
-                      top: (1.5*_animation.value.toDouble())+200.0,
-                    ),
-                    child: new Column(
-                      children: <Widget>[
-                        new Container(
-                          child: new Row(
-                            children: <Widget>[
-                              new Expanded(
-                                child: new TitleBar(title: widget.word),
-                              ),
-                            ],
+                  new GestureDetector(
+                    onVerticalDragUpdate: (details) {
+                      setState(() {
+                        _dragDelta += details.delta;
+                      });
+                    },
+                    child: new Container(
+                      margin: new EdgeInsets.only(
+                        top: (1.5 * _animation.value.toDouble()) + 200.0 +
+                            (_dragDelta != null ? _dragDelta.dy : 0.0),
+                      ),
+                      child: new Column(
+                        children: <Widget>[
+                          new Container(
+                            child: new Row(
+                              children: <Widget>[
+                                new Expanded(
+                                  child: new TitleBar(title: widget.word),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        new Container(
-                          margin: new EdgeInsets.only(
-                            top: 20.0,
-                            left: 100.0,
+                          new Container(
+                            margin: new EdgeInsets.only(
+                              top: 20.0,
+                              left: 100.0,
+                            ),
+                            child: new Definitions(word: widget.word),
                           ),
-                          child: new Definitions(word: widget.word),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ],
